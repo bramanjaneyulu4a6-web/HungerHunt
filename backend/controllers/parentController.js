a637256cfb9c191e9a7af779d8d4c7c83d3db006
bramanjaneyulu4a6-web <bramanjaneyulu4a6@gmail.com>
2026-08-14 15:33:18 +0530

    fp2

--- backend/controllers/parentController.js
+++ backend/controllers/parentController.js
@@ -1110,12 +1110,20 @@ export const forgotPassword = async (req, res) => {
 const resetUrl =
   `${process.env.PARENT_FRONTEND_URL}/reset-password/${resetToken}`;
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
+
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+
+  tls: {
+    minVersion: "TLSv1.2",
+  },
+});
 
     await transporter.sendMail({
       to: parent.email,
